af2e5ed1b2530d7bf12806ff262ccc60ffe8d224
sumashu <sumitpote584@gmail.com>
2023-02-22 09:35:24 +0530

    inital commit

---
+++ src/components/Hello.js
@@ -0,0 +1,8 @@
+//  function Hello (){
+//     return <h1>Hello World!</h1>
+//  }
+const name = "Ashutosh";
+const Hello = ()=> 
+{return <h1>Hello India! my name is {name}</h1>}
+
+ export default Hello;
---
+++ src/components/Profile.js
@@ -0,0 +1,7 @@
+ const Profile=(Props)=>{
+    return (<h1>My name is {Props.name} {Props.lastName} {Props.children}</h1>
+             
+    )
+ }
+
+ export default Profile
